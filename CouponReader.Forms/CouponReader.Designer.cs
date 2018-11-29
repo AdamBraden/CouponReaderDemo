b90ae593c2da2cb7c44ddfa8917499bd12bfe523
Adam Braden <adambr@microsoft.com>
2018-11-28 19:00:04 -0800

    Remove unnecessary login controls

--- CouponReader.Forms/CouponReader.Designer.cs
+++ CouponReader.Forms/CouponReader.Designer.cs
@@ -37,10 +37,6 @@
             this.scanButton = new System.Windows.Forms.Button();
             this.loginPanel = new System.Windows.Forms.Panel();
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.label2 = new System.Windows.Forms.Label();
-            this.textBox1 = new System.Windows.Forms.TextBox();
-            this.label1 = new System.Windows.Forms.Label();
             this.button1 = new System.Windows.Forms.Button();
             this.couponHistoryControl1 = new CouponReaderDemo.Forms.CouponHistoryControl();
             this.couponDetailsControl1 = new CouponReaderDemo.Forms.CouponDetailsControl();
@@ -131,13 +127,9 @@
             // loginPanel
             // 
             this.loginPanel.Controls.Add(this.pictureBox1);
-            this.loginPanel.Controls.Add(this.textBox2);
-            this.loginPanel.Controls.Add(this.label2);
-            this.loginPanel.Controls.Add(this.textBox1);
-            this.loginPanel.Controls.Add(this.label1);
             this.loginPanel.Controls.Add(this.button1);
             this.loginPanel.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.loginPanel.Location = new System.Drawing.Point(85, 289);
+            this.loginPanel.Location = new System.Drawing.Point(24, 261);
             this.loginPanel.Name = "loginPanel";
             this.loginPanel.Size = new System.Drawing.Size(722, 354);
             this.loginPanel.TabIndex = 9;
@@ -151,41 +143,9 @@
             this.pictureBox1.TabIndex = 5;
             this.pictureBox1.TabStop = false;
             // 
-            // textBox2
-            // 
-            this.textBox2.Location = new System.Drawing.Point(365, 279);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(127, 26);
-            this.textBox2.TabIndex = 4;
-            // 
-            // label2
-            // 
-            this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(272, 279);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(82, 20);
-            this.label2.TabIndex = 3;
-            this.label2.Text = "Password:";
-            // 
-            // textBox1
-            // 
-            this.textBox1.Location = new System.Drawing.Point(365, 241);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(127, 26);
-            this.textBox1.TabIndex = 2;
-            // 
-            // label1
-            // 
-            this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(272, 241);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(87, 20);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "Username:";
-            // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(300, 331);
+            this.button1.Location = new System.Drawing.Point(348, 286);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(156, 33);
             this.button1.TabIndex = 0;
@@ -240,10 +200,6 @@
         private global::CouponReaderDemo.Forms.CouponDetailsControl couponDetailsControl1;
         private global::CouponReaderDemo.Forms.CouponHistoryControl couponHistoryControl1;
         private System.Windows.Forms.Panel loginPanel;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.Label label2;
-        private System.Windows.Forms.TextBox textBox1;
-        private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.PictureBox pictureBox1;
     }
